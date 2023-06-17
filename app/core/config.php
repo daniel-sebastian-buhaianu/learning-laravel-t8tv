84686749d9e50fa999fe1913b02e42d03cca6eea
Daniel-Sebastian Buhaianu <dsb99.dev@gmail.com>
2023-06-17 16:24:48 +0300

    laravel boilerplate code

--- app/core/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-define('APP_NAME', 'vidapp');
-
-define('ROOT_PATH', '/'.APP_NAME);
-
-define('BASE_URL', 'http://localhost'.ROOT_PATH);
--- app/core/init.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-require __DIR__.'/config.php';
-require __DIR__.'/helper_functions.php';
-require __DIR__.'/../controllers/home.controller.php';
-require __DIR__.'/../controllers/categories.controller.php';
-require __DIR__.'/../controllers/category.controller.php';
-require __DIR__.'/../controllers/page-not-found.controller.php';
-require __DIR__.'/App.php';
