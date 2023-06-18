e82b611b165eb0606ff3308842d7bc064e4c5387
Daniel-Sebastian Buhaianu <dsb99.dev@gmail.com>
2023-06-18 08:48:00 +0300

    created essential migrations

---
+++ database/migrations/2023_06_18_050254_create_rumble_channel_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rumble_channel', function (Blueprint $table) {
+            $table->id('rumble_channel_id');
+            $table->string('id', 127)->unique();
+            $table->string('url', 255)->unique();
+            $table->string('title', 127)->unique();
+            $table->date('joining_date');
+            $table->text('description')->nullable();
+            $table->string('banner', 255)->nullable();
+            $table->string('avatar', 255)->nullable();
+            $table->unsignedBigInteger('followers_count')->nullable();
+            $table->integer('videos_count')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rumble_channel');
+    }
+};
